f3146bbeaf1ab33f0ec7edf0c4be4650cdffdf13
078raiyaresturabbani-cmyk <078raiyaresturabbani@gmail.com>
2025-12-07 21:17:37 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -778,7 +778,7 @@ end
 -- helper bikin CARD quest
 local function MakeQuestCard(parent, titleText, subText)
     local card = Instance.new("Frame")
-    card.Size = UDim2.new(1, -4, 0, 90) -- tinggi card, muat 2 row
+    card.Size = UDim2.new(1, -4, 0, 82) -- tinggi card, muat 2 row
     card.BackgroundColor3 = Color3.fromRGB(18, 20, 44)
     card.BackgroundTransparency = 0.1
     card.BorderSizePixel = 0
